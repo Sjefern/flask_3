79502290c9d2692985a3522e19d788dfa522a852
Sjefern <sebastiankjos08@gmail.com>
2025-09-24 11:03:29 +0200

    fiksa alt ts

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+        <h1>velkommen til denne nettsiden</h1>
+        <p><a href="https://www.vg.no/">Gå til VG</a></p>
+        <p><a href="/aboutme/sebastian">Les mer om meg</a></p>
+        <p><a href="/aboutbike/sebastian">Les mer om sykkel</a></p>
+        <p><a href="/aboutlife/sebastian">Les mer om livet</a></p>
+    '''
+
+@app.route('/aboutme/<navn>')
+def aboutme(navn):
+    return f'''
+        <h1>Om meg</h1>
+        <p>Hei, jeg heter {navn}.</p>
+        <a href="/">Tilbake til forsiden</a>
+    '''
+
+@app.route('/aboutbike/<navn>')
+def aboutbike(navn):
+    return f'''
+        <h1>Om sykkel</h1>
+        <p>Hei, jeg heter {navn}. Her skriver jeg om sykler.</p>
+        <a href="/">Tilbake til forsiden</a>
+    '''
+
+@app.route('/aboutlife/<navn>')
+def aboutlife(navn):
+    return f'''
+        <h1>Om livet</h1>
+        <p>Hei, jeg heter {navn}. Her skriver jeg om livet.</p>
+        <a href="/">Tilbake til forsiden</a>
+    '''
+
+if __name__ == "__main__":
+    app.run()
+
